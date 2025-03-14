c7e6b915e0782399399a0c838ad4742d2fdbe172
Rodgence <rodgencekagaba@yahoo.com>
2025-03-14 09:56:12 +0300

    logo adjusted for mobile view to be smaller

--- includes/header.php
+++ includes/header.php
@@ -5,43 +5,24 @@
     <meta charset="utf-8">
     <title>Sabmeith Logistics</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <meta content="" name="keywords">
-    <meta content="" name="description">
-
-    <!-- Favicon -->
-    <link href="img/favicon.ico" rel="icon">
-
-    <!-- Google Web Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&family=Roboto:wght@500;700&display=swap" rel="stylesheet">
-
-    <!-- Icon Font Stylesheet -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
-
-    <!-- Libraries Stylesheet -->
-    <link href="lib/animate/animate.min.css" rel="stylesheet">
-    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
-
-    <!-- Font Awesome for chevron icons -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-
-    <!-- Bootstrap Stylesheet -->
+    
+    <!-- Bootstrap CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Custom Stylesheets -->
+    <!-- Custom Styles -->
     <link href="css/style.css" rel="stylesheet">
     <link href="css/styles.css" rel="stylesheet">
 
-    <!-- Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'G-XXXXXXXXXX');
-    </script>
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
+    
+    <!-- jQuery (Required for Bootstrap JS) -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
+
 </head>
 
 <body>
@@ -51,9 +32,12 @@
         <a href="index.php" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
             <img src="img/loogo.png" alt="Sabmeith Logistics Logo">
         </a>
-        <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+
+        <!-- Hamburger Button -->
+        <button class="navbar-toggler me-4" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
+
         <div class="collapse navbar-collapse" id="navbarCollapse">
             <div class="navbar-nav ms-auto p-4 p-lg-0">
                 <a href="index.php" class="nav-item nav-link active">Home</a>
@@ -71,6 +55,7 @@
                 <a href="contact.php" class="nav-item nav-link">Contact</a>
                 <a href="inquiry.php" class="nav-item nav-link">Inquiry</a>
             </div>
+
             <h4 class="m-0 pe-lg-5 d-none d-lg-block">
                 <i class="fa fa-headphones text-primary me-3"></i>
                 <a href="https://wa.me/255775999000">+255 775 999 000</a>
@@ -78,6 +63,18 @@
         </div>
     </nav>
     <!-- Navbar End -->
+
+    <script>
+        // Close navbar when clicking outside on mobile
+        $(document).click(function (event) {
+            var clickover = $(event.target);
+            var navbarCollapse = $("#navbarCollapse");
+            var opened = navbarCollapse.hasClass("show");
+            if (opened && !clickover.closest(".navbar").length) {
+                $(".navbar-toggler").click();
+            }
+        });
+    </script>
 
 </body>
 </html>
